cbfa2d4213e4712818691887d6bc1ddca620de68
谢正云 <1549303595@qq.com>
2020-05-04 10:05:46 +0800

    file-loader

--- src/search.js
+++ src/search.js
@@ -2,12 +2,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './search.less';
+import './assets/css/search.less';
+import audio_ico from './assets/images/audio_ico.png';
+import browser from './assets/images/browser.jpg';
 
 class Search extends React.Component{
 
     render(){
-        return <div class="search-text">Search Text</div>
+        return <div class="search-text">
+                 搜索页面
+                <img src={ browser } class="browser"/>
+                <img src={ audio_ico } />
+            </div>
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,25 @@ module.exports = {
                     'less-loader',//先less-loader转译，然后执行css-loader解释css，再传递给style-loader写到页面head
                 ]
             },
+            // {
+            //     test: /\.(png|svg|jpg|jpeg|gif)$/,
+            //     use:'file-loader'
+            // },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader', //设置limit 小于 10k  base64转码
+                        options:{
+                            limit: 10240
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|TTF|otf)$/, //大小写区分
+                use:'file-loader'
+            }
         ]
     },
 }
